6f4315cab3b9c4aba03fb01b4d1bc8cb7a19304d
sampalu <andreluisampaio@gmail.com>
2025-02-02 19:30:59 -0300

    Código-fonte do site Hugo deploy

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Gera os arquivos estáticos
+hugo --minify
+
+# Entra na pasta "public", que contém o site gerado
+cd public
+
+# Configura o branch gh-pages
+git init
+git remote add origin https://github.com/sampalu/guia-de-estudos.git
+git checkout -b gh-pages
+
+# Adiciona e faz commit dos arquivos gerados
+git add .
+git commit -m "Atualizando site"
+
+# Força o push para sobrescrever o branch gh-pages
+git push --force origin gh-pages
+
+# Volta para a pasta raiz
+cd ..
+
